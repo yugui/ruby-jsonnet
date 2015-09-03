ee1c4e625efd72c3042faab0e58d6a70129565ff
Yuki Yugui Sonoda <yugui@yugui.jp>
2015-09-04 01:09:20 +0900

    Fix packaging
    Correctly build the ext on installation of the gem

--- ext/jsonnet/extconf.rb
+++ ext/jsonnet/extconf.rb
@@ -3,4 +3,4 @@ require 'mkmf'
 dir_config('jsonnet')
 abort 'libjsonnet.h not found' unless have_header('libjsonnet.h')
 abort 'libjsonnet not found' unless have_library('jsonnet')
-create_makefile('jsonnet_wrap')
+create_makefile('jsonnet/jsonnet_wrap')
