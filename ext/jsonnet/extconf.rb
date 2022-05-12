6b475aa20d97bd8688b54eaf2324213b97adc436
Yuki Yugui Sonoda <yugui@yugui.jp>
2022-05-12 21:45:08 +0900

    Fix runtime load error
    Link objects that had added to Jsonnet

--- ext/jsonnet/extconf.rb
+++ ext/jsonnet/extconf.rb
@@ -16,6 +16,25 @@ unless using_system_libraries?
   recipe = MiniPortile.new('jsonnet', 'v0.18.0')
   recipe.files = ['https://github.com/google/jsonnet/archive/v0.18.0.tar.gz']
   class << recipe
+    CORE_OBJS = %w[
+      desugarer.o formatter.o lexer.o libjsonnet.o parser.o pass.o static_analysis.o string_utils.o vm.o
+    ].map {|name| File.join('core', name) }
+    MD5_OBJS = %w[
+      md5.o
+    ].map {|name| File.join('third_party', 'md5', name) }
+    C4_CORE_OBJS = %w[
+      base64.o
+      char_traits.o
+      error.o
+      format.o
+      language.o
+      memory_resource.o
+      memory_util.o
+      time.o
+    ].map {|name| File.join('third_party', 'rapidyaml', 'rapidyaml', 'ext', 'c4core', 'src', 'c4', name) }
+    RAPID_YAML_OBJS = %w[
+      common.o parse.o preprocess.o tree.o
+    ].map {|name| File.join('third_party', 'rapidyaml', 'rapidyaml', 'src', 'c4', 'yml', name) }
 
     def compile
       # We want to create a file a library we can link to. Jsonnet provides us
@@ -24,7 +43,7 @@ unless using_system_libraries?
       # we compile the c into .o files and then create an archive that can
       # be linked to
       execute('compile', make_cmd)
-      execute('archive', 'ar rcs libjsonnet.a core/desugarer.o core/formatter.o core/lexer.o core/libjsonnet.o core/parser.o core/pass.o core/static_analysis.o core/string_utils.o core/vm.o third_party/md5/md5.o')
+      execute('archive', 'ar rcs libjsonnet.a ' + target_object_files.join(' '))
     end
 
     def configured?
@@ -41,6 +60,15 @@ unless using_system_libraries?
       FileUtils.cp(File.join(work_path, 'include', 'libjsonnet.h'), include_path)
       FileUtils.cp(File.join(work_path, 'include', 'libjsonnet_fmt.h'), include_path)
     end
+
+    private
+    def target_object_files
+      if version >= 'v0.18.0'
+        CORE_OBJS + MD5_OBJS + C4_CORE_OBJS + RAPID_YAML_OBJS
+      else
+        CORE_OBJS + MD5_OBJS
+      end
+    end
   end
 
   recipe.cook
@@ -48,12 +76,6 @@ unless using_system_libraries?
   # but the makefile to fail. These commands add the necessary paths to do both
   $LIBPATH = ["#{recipe.path}/lib"] | $LIBPATH
   $CPPFLAGS << " -I#{recipe.path}/include"
-
-  # This resolves an issue where you can get improper linkage when compiling
-  # and get an error like "undefined symbol: _ZTVN10__cxxabiv121__vmi_class_type_infoE"
-  # experienced on ubuntu.
-  # See: https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=193950
-  $LIBS << " -lstdc++"
 end
 
 abort 'libjsonnet.h not found' unless have_header('libjsonnet.h')
